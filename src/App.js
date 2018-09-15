e04afec69c6874757f7d30361759c708bf34f4da
Kasem Wongsa <zero71st@gmail.com>
2018-09-15 22:35:13 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Todo Application</h1>
         </header>
         <p className="App-intro">
-          <TodoForm/>
+          {/* <TodoForm/> */}
         </p>
       </div>
     );
